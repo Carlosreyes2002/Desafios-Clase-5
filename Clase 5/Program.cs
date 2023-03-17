d0dfb8664f0bc4d7626e1fdf050653ffe3dedfca
Carlos Reyes <carlosrrg2002@gmail.com>
2023-03-17 16:02:52 -0600

    subinedo los archivo Clase 5

---
+++ Clase 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class program{
+    static void Main(string[]args) {
+        Console.Write("Ingrese numero: ");
+        int num = int.Parse(Console.ReadLine());
+        bool siesprimo = true; 
+
+        for (int i = 2; i<num; i++ ) {
+            if(num % i==0) {
+                siesprimo = false;
+                break;
+            }
+        } 
+      
+      if (siesprimo) {
+        Console.WriteLine(num + " Es numero primo");
+
+      } else {
+        Console.WriteLine(num + " No es numero primo");
+      }
+
+    
+    }
+}
---
+++ EJercicio 1 clase 5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class program{
+    static void Main(string[]args) {
+        Console.Write("Ingrese numero: ");
+        int num = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i<= num ; i++) {
+            if (i % 2 == 0) {
+                Console.WriteLine(i);
+            }
+        }
+
+    
+    }
+}
